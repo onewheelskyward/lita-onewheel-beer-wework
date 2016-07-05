24205b3450ebccfcadf6028fd7af18980de6ae92
Andrew Kreps <andrew.kreps@gmail.com>
2016-07-05 14:52:56 -0700

    Refactored the storage to contain user and time, added json as a dep.  Not sure if the lack of it in the gemspec will be a problem or not.

--- lib/lita/handlers/onewheel_beer_wework.rb
+++ lib/lita/handlers/onewheel_beer_wework.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module Lita
   module Handlers
     class OnewheelBeerWework < Handler
@@ -5,26 +7,26 @@ module Lita
       LOCATIONS = %w(1fs 1fn 2fs 2fn 3fn)
 
       route /^wework\s+([\w\/+_-]+)\s+(.*)$/i,
-            :command_add_key,
+            :command_add_beer,
             command: true,
             help: {'!wework 2fS Beer' => 'Add a beer to 2fS'}
       route /^wework$/i,
-            :command_list_keys,
+            :command_list_beers,
             command: true,
             help: {'!wework            ' => 'list all beers'}
       route /^wework\s+(\w+)$/i,
-            :command_fetch_key,
+            :command_fetch_beer,
             command: true,
             help: {'!wework 2fS        ' => 'fetch the beer for 2fS'}
 
-      def command_add_key(response)
+      def command_add_beer(response)
         key = response.matches[0][0].downcase
         unless LOCATIONS.include? key
           Lita.logger.info "#{key} was not found in the floors array."
           return
         end
         beer_name = response.matches[0][1]
-        key = key[0..1] + key[-1].upcase
+        key = key[0..1] + key[-1].downcase
         value = {
             name: beer_name,
             user: response.user.name,
@@ -34,25 +36,30 @@ module Lita
         response.reply "Logged #{beer_name} at #{key}!"
       end
 
-      def command_fetch_key(response)
+      def command_fetch_beer(response)
         key = response.matches[0][0]
 
         values = get_values_that_start_with_key(key)
         values.each do |keg|
-          floor_id = keg.keys[0]
-          keg_meta = JSON.parse keg[floor_id]
-          response.reply "#{floor_id}: #{keg_meta['name']}"
+          floor_id, keg_name = get_floor_and_keg_name(keg)
+          response.reply "#{floor_id}: #{keg_name}"
         end
       end
 
-      def command_list_keys(response)
-        replies = []
+      def get_floor_and_keg_name(keg)
+        floor_id = keg.keys[0]
+        keg_meta = JSON.parse(keg[floor_id])
+        return floor_id, keg_meta['name']
+      end
 
-        all = redis.hgetall(REDIS_KEY)
-        all.each do |key, val|
-          replies.push format_key_val_response(key, val)
+      def command_list_beers(response)
+        test = redis.hgetall(REDIS_KEY)
+        LOCATIONS.each do |floor|
+          if (data = redis.hget(REDIS_KEY, floor))
+            floor, keg_name = get_floor_and_keg_name(floor => data)
+            response.reply "#{floor}: #{keg_name}"
+          end
         end
-        response.reply replies.join ', '
       end
 
       def get_values_that_start_with_key(key)
@@ -64,10 +71,6 @@ module Lita
           end
         end
         values
-      end
-
-      def format_key_val_response(all_key, all_val)
-        "#{all_key}: #{all_val}"
       end
     end
 
--- spec/lita/handlers/onewheel_beer_wework_spec.rb
+++ spec/lita/handlers/onewheel_beer_wework_spec.rb
@@ -6,20 +6,25 @@ describe Lita::Handlers::OnewheelBeerWework, lita_handler: true do
 
   it 'sets a beer' do
     send_command 'wework 2fs Breakfast Stout'
-    expect(replies.last).to eq('Logged Breakfast Stout at 2fS!')
+    expect(replies.last).to eq('Logged Breakfast Stout at 2fs!')
+  end
+
+  it 'doesn\'t set a beer' do
+    send_command 'wework 9fF Breakfast Stout'
+    expect(replies.last).to eq(nil)
   end
 
   it 'gets a beer' do
     send_command 'wework 2fs Breakfast Stout'
     send_command 'wework 2fs'
-    expect(replies.last).to eq('2fS: Breakfast Stout')
+    expect(replies.last).to eq('2fs: Breakfast Stout')
   end
 
-  it 'lists documents' do
+  it 'lists beers' do
     send_command 'wework 2fs one'
     send_command 'wework 2fN two'
     send_command 'wework'
-    expect(replies[2]).to eq('2fS: one')
-    expect(replies.last).to eq('2fN: two')
+    expect(replies[2]).to eq('2fs: one')
+    expect(replies.last).to eq('2fn: two')
   end
 end
